a63a4b33849048b8923e110ee0b3729696277b98
Developer Hasan <67751842+DeveloperHasanBD@users.noreply.github.com>
2023-05-26 10:47:14 +0600

    Create function.php

---
+++ function.php
@@ -0,0 +1,31 @@
+
+if (!function_exists('extend_admin_search')) {
+    add_action('admin_init', 'extend_admin_search');
+
+    function extend_admin_search() {
+        global $typenow;
+
+        if ($typenow === 'cards') {
+            add_filter('posts_search', 'posts_search_custom_post_type', 10, 2);
+        }
+    }
+
+    function posts_search_custom_post_type($search, $query) {
+        global $wpdb;
+
+        if ($query->is_main_query() && !empty($query->query['s'])) {
+            $sql    = "
+            or exists (
+                select * from {$wpdb->postmeta} where post_id={$wpdb->posts}.ID
+                and meta_key in ('aci_card_number')
+                and meta_value like %s
+            )
+        ";
+            $like   = '%' . $wpdb->esc_like($query->query['s']) . '%';
+            $search = preg_replace("#\({$wpdb->posts}.post_title LIKE [^)]+\)\K#",
+                $wpdb->prepare($sql, $like), $search);
+        }
+
+        return $search;
+    }
+}
